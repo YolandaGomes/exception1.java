d3662e31633100631822a9bd06f8ee2df35f6b26
YolandaGomes <yolandar.gomes@gmail.com>
2023-08-25 14:28:48 -0300

    first commit

---
+++ src/aplication/Program.java
@@ -0,0 +1,50 @@
+package aplication;
+
+import model.entities.Reservation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.concurrent.locks.ReadWriteLock;
+
+public class Program {
+    public static void main(String[] args) throws ParseException {
+
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Room number: ");
+        int number = sc.nextInt();
+        System.out.print("Check in date: (dd/MM/yyyy) ");
+        Date checkin = sdf.parse(sc.next());
+        System.out.print("Check Out date: (dd/MM/yyyy) ");
+        Date checkout = sdf.parse(sc.next());
+
+        if (!checkout.after(checkin)){
+            System.out.println(" Error in reservation, Check out mais cedo que check in");
+        } else {
+            Reservation reservation = new Reservation(number, checkin, checkout);
+            System.out.println(reservation);
+            System.out.println();
+            System.out.println("Entre com o update da reserva: ");
+            System.out.print("Check in date: (dd/MM/yyyy) ");
+            checkin = sdf.parse(sc.next());
+            System.out.print("Check Out date: (dd/MM/yyyy) ");
+            checkout = sdf.parse(sc.next());
+
+            Date now = new Date();
+            if (checkin.before(now) || checkout.before(now)){
+                System.out.println(" Error in reservation, Check out e check in precisam ser datas futuras");
+            } else {
+                if (!checkout.after(checkin)){
+                    System.out.println(" Error in reservation, Check out mais cedo que check in");
+                } else {
+                    reservation.updateDate(checkin, checkout);
+                    System.out.println(reservation);
+                }
+            }
+        }
+        System.out.println();
+    }
+}
